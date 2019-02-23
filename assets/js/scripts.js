c60984dd199c2abcbf7816112f207d4a44f7a6e2
Thomas Jones <tahjones75@gmail.com>
2019-02-23 09:32:21 +0000

    resume-mini-project - added google maps api with cluster markers

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -74,3 +74,36 @@ var copyRightText = "2018 - " + year + " © Thomas Jones - All Rights Reserved";
 
 // insert copyright statement into copyRight div
 copyRight.innerHTML = copyRightText;
+
+
+// add google map with cluster markers
+function initMap() {
+  var map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 3,
+    center: {
+      // lat: 46.1619261,
+      // lng: -33.134766
+      lat: 40.785091,
+      lng: -73.96825
+    }
+  });
+
+  var labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+  var locations = [
+    { lat: 40.785091, lng: -73.968285 },
+    { lat: 41.084045, lng: -73.874245 },
+    { lat: 40.754932, lng: -73.984016 }
+  ];
+
+  var markers = locations.map(function(location, i) {
+    return new google.maps.Marker({
+      position: location,
+      label: labels[i % labels.length]
+    });
+  });
+
+  // Add a marker clusterer to manage the markers.
+  var markerCluster = new MarkerClusterer(map, markers, { imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m' });
+
+}

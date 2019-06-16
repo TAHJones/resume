9b5c80d863cf643b290a8fbfe9d2a5f1b554c35e
Thomas Jones <tahjones75@gmail.com>
2019-06-16 13:01:48 +0000

    resume-mini-project - update sendEmail.js to reflect changes made to emailjs service

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -1,21 +1,24 @@
 
-// target form element and input values
-var contactForm = document.getElementById('contactForm');
-var formName = contactForm.elements.namedItem('name').value;
-var formEmail = contactForm.elements.namedItem('email').value;
-var formMsg = contactForm.elements.namedItem('msg').value;
 
-// insert input value variables into templateParams variable in json format
-var templateParams = {
-    "from_name": formName,
-    "from_email": formEmail,
-    "message_from_online_resume": formMsg
-};
-// console.log(0, templateParams);
 
 // on submit event send input values to emailjs template
 window.onload = function() {
     contactForm.addEventListener('submit', function(event) {
+
+        // target form element and input values
+        var contactForm = document.getElementById('contactForm');
+        var formName = contactForm.elements.namedItem('name').value;
+        var formEmail = contactForm.elements.namedItem('email').value;
+        var formMsg = contactForm.elements.namedItem('msg').value;
+        
+        // insert input value variables into templateParams variable in json format
+        var templateParams = {
+            "from_name": formName,
+            "from_email": formEmail,
+            "message_from_online_resume": formMsg
+        };
+        // console.log(0, templateParams);
+
         event.preventDefault();
         // console.log(1, formMsg);
 
@@ -26,10 +29,10 @@ window.onload = function() {
             // console.log(2, templateParams);
             console.log('SUCCESS!', response.status, response.text);
             // if message is successfully submitted reset form input fields
-            /*if(response.status === 200) {
-                console.log(3, templateParams);
-                contactForm.reset();
-           }*/
+            if(response.status === 200) {
+                // console.log(3, templateParams);
+                // contactForm.reset();
+          }
         }, function(error) {
            console.log('FAILED...', error);
         });
